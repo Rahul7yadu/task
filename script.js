6bce685a67a529ccd0e556382a819c2af8a0d85c
Rahul Yadu <rahulyadu17@gmail.com>
2024-12-16 14:13:01 +0530

    some improvements

--- script.js
+++ script.js
@@ -10,11 +10,15 @@ document.addEventListener("DOMContentLoaded", () => {
         const option = b.querySelector(".options-wrapper");
         option.classList.remove("show");
         console.log(option);
+        const radioButton = b.querySelector(".checkbox");
+        radioButton.checked = false;
       });
       // Add 'expanded' class to the clicked box
       box.classList.add("expanded");
       const options = box.querySelector(".options-wrapper");
       options.classList.add("show");
+      const checkbox = box.querySelector(".checkbox");
+      checkbox.checked = true;
       //   console.log(options);
     });
   });
